ce1765744c88e0b706aec35f6c9a042f1e08eef3
rubicus <laba625@yandex.ru>
2024-11-19 10:27:55 +0300

    idk2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loadingScreen = document.querySelector(".loading-screen");
 
-    // loadingScreen.style.opacity = "0";
+    loadingScreen.style.opacity = "0";
 
     setTimeout(() => {
         loadingScreen.style.opacity = "0";
@@ -20,20 +20,43 @@ var isMenuOpened = false;
 var menuBtn = document.getElementById("menu-btn");
 var mobileHeaderOverlay = document.getElementById("mobile-header-overlay");
 menuBtn.addEventListener("onclick", onMenuClick);
-console.log(menuBtn);
+
+document.addEventListener("keydown", (event) => {
+    if ((event.key = "Escape")) hideMobileOverlay();
+});
 
 function onMenuClick() {
-    isMenuOpened = !isMenuOpened;
-
-    if (isMenuOpened) {
-        document.getElementsByTagName("html")[0].style.overflow = "hidden";
-
-        mobileHeaderOverlay.style.display = "block";
-        menuBtn.src = closeSrc;
-    } else {
-        document.getElementsByTagName("html")[0].style.overflow = "visible";
-
-        mobileHeaderOverlay.style.display = "none";
-        menuBtn.src = menuSrc;
-    }
+    if (!isMenuOpened) showMobileOverlay();
+    else hideMobileOverlay();
 }
+
+function showMobileOverlay() {
+    isMenuOpened = true;
+
+    document.getElementsByTagName("html")[0].style.overflow = "hidden";
+    mobileHeaderOverlay.style.display = "block";
+    menuBtn.src = closeSrc;
+}
+
+function hideMobileOverlay() {
+    isMenuOpened = false;
+
+    document.getElementsByTagName("html")[0].style.overflow = "visible";
+    mobileHeaderOverlay.style.display = "none";
+    menuBtn.src = menuSrc;
+}
+
+// script.js
+document.addEventListener("scroll", function () {
+    const parallaxContainer = document.querySelector(".overflow-container");
+    const parallaxContent = document.querySelector("#text1");
+    const scrollPosition = window.scrollY;
+    const containerOffsetTop = parallaxContainer.offsetTop;
+    const containerHeight = parallaxContainer.offsetHeight;
+
+    if (scrollPosition >= containerOffsetTop && scrollPosition <= containerOffsetTop + containerHeight) {
+        const parallaxSpeed = 0.5; // Скорость параллакса
+        const parallaxOffset = (scrollPosition - containerOffsetTop) * parallaxSpeed;
+        parallaxContent.style.transform = translateY(`${parallaxOffset}px`);
+    }
+});
