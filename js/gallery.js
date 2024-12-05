7273824560c0a1038052750173c8048d9c57432e
rubicus <laba625@yandex.ru>
2024-12-05 12:05:31 +0300

    мууки

--- js/gallery.js
+++ js/gallery.js
@@ -1,9 +1,5 @@
 var currentPage = 0;
 
-function randomInteger(min, max) {
-    let rand = min + Math.random() * (max + 1 - min);
-    return Math.floor(rand);
-}
 async function getCollection(page = 1) {
     let media_type = "image";
     let keywords;
